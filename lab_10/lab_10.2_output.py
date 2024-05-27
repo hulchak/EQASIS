3df1de26c7522ba2f99dd6fcc9d186dc7b29312f
Anatolii Hulchak <107542294+hulchak@users.noreply.github.com>
2024-05-27 20:34:09 +0300

    Update lab_10.2_output.py

--- lab_10/lab_10.2_output.py
+++ lab_10/lab_10.2_output.py
@@ -1,4 +1,5 @@
 class LibraryBook:
+
     def __init__(self, title, author, publication_year):
         self.title = title
         self.author = author
@@ -17,14 +18,47 @@ class LibraryBook:
             return True
         return False
 
+class BookDetails:
+
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.condition = "Good"
+
     def get_info(self):
         return f"Title: {self.title}, Author: {self.author}, Year: {self.publication_year}"
 
-    def is_available(self):
-        return not self.is_checked_out
+    def check_condition(self):
+        return self.condition
 
-book = LibraryBook("The Great Gatsby", "F. Scott Fitzgerald", 1925)
-print(book.get_info())
-print("Available:", book.is_available())
-print("Check out:", book.check_out())
-print("Available after check out:", book.is_available())
+class EnhancedLibraryBook:
+
+    def __init__(self, title, author, publication_year):
+        self.book_details = BookDetails(title, author, publication_year)
+        self.is_checked_out = False
+
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return True
+        return False
+
+    def get_book_info(self):
+        return self.book_details.get_info()
+
+    def get_book_condition(self):
+        return self.book_details.check_condition()
+
+# Використання коду
+enhanced_book = EnhancedLibraryBook("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+print(enhanced_book.check_out())
+print(enhanced_book.check_out())
+print(enhanced_book.get_book_info())
+print(enhanced_book.get_book_condition())
